90d190fd321fb332304a2072f39bd80f2dea17b0
lfnt <jmccarthy91@gmail.com>
2023-03-10 13:15:25 -0700

    fixed end game logic

--- QuadJam3/Assets/Scripts/UI/EndMenuDisplayTime.cs
+++ QuadJam3/Assets/Scripts/UI/EndMenuDisplayTime.cs
@@ -9,9 +9,16 @@ public class EndMenuDisplayTime : MonoBehaviour
     {
         float time = GameManager.Instance.inGameTime;
         int day = (int)(time) % 30;
-        int month = ((int)(time) / 30);
+        int month = ((int)(time) / 30) % 12;
         int year = (int)(time) / 365;        
 
-        transform.GetComponent<Text>().text = "You got home after " + month + " months or " + year + " years.";
+        if (year <= 50)
+        {
+            transform.GetComponent<Text>().text = "You made it home after only " + year + " years and " + month + " months.\nCongratulations, your wife is still alive!";
+        }
+        else
+        {
+            transform.GetComponent<Text>().text = "You made it home after only " + year + " years and " + month + " months.\nYour wife has died of old age...";
+        }
     }
 }
