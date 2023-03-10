33b4c63a785fe409c7e266c3bd159778e58239cb
Sergio Lecuona <serlecu@hotmail.com>
2023-03-10 16:34:09 +0100

    create main menu UI manager
    event for opening settings and credits

---
+++ QuadJam3/Assets/Scripts/UI/MainMenuUIManager.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class MainMenuUIManager : MonoBehaviour
+{
+    public static MainMenuUIManager Instance;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnDestroy()
+    {
+        if (Instance == this)
+        {
+            Instance = null;
+        }
+    }
+
+    public event Action onSettingsOpen;
+    public void OnSettingsOpen()
+    {
+      if (onSettingsOpen != null)
+      {
+        onSettingsOpen();
+      }
+    }
+
+    public event Action onSettingsClose;
+    public void OnSettingsClose()
+    {
+      if (onSettingsClose != null)
+      {
+        onSettingsClose();
+      }
+    }
+
+    public event Action onCreditsOpen;
+    public void OnCreditsOpen()
+    {
+      if (onCreditsOpen != null)
+      {
+        onCreditsOpen();
+      }
+    }
+
+    public event Action onCreditsClose;
+    public void OnCreditsClose()
+    {
+      if (onCreditsClose != null)
+      {
+        onCreditsClose();
+      }
+    }
+}
