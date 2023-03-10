320fb047f3ac7788e049ac14167ae4d4592f9cd7
lfnt <jmccarthy91@gmail.com>
2023-03-10 13:28:07 -0700

    tweaked game endings

--- QuadJam3/Assets/Scripts/UI/EndMenuDisplayTime.cs
+++ QuadJam3/Assets/Scripts/UI/EndMenuDisplayTime.cs
@@ -7,18 +7,16 @@ public class EndMenuDisplayTime : MonoBehaviour
 {
     void Start()
     {
-        float time = GameManager.Instance.inGameTime;
-        int day = (int)(time) % 30;
-        int month = ((int)(time) / 30) % 12;
+        float time = GameManager.Instance.inGameTime * 80;
         int year = (int)(time) / 365;        
 
         if (year <= 50)
         {
-            transform.GetComponent<Text>().text = "You made it home after only " + year + " years and " + month + " months.\nCongratulations, your wife is still alive!";
+            transform.GetComponent<Text>().text = "You made it home after just " + year + " years. Congratulations, your wife is still alive!";
         }
         else
         {
-            transform.GetComponent<Text>().text = "You made it home after only " + year + " years and " + month + " months.\nYour wife has died of old age...";
+            transform.GetComponent<Text>().text = "You made it home after " + year + " long years. Your wife has died of old age...";
         }
     }
 }
