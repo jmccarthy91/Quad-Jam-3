16e38c6f2fc14394d1dab9a4b4a3059bda5eab1f
Sergio Lecuona <serlecu@hotmail.com>
2023-03-12 08:26:00 +0100

    modularized modalMainMenu ButtonCreddits and ModalCredits

--- QuadJam3/Assets/Scripts/UI/ModalCredits.cs
+++ QuadJam3/Assets/Scripts/UI/ModalCredits.cs
@@ -1,19 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ModalCredits : MonoBehaviour
 {
     void Start()
     {
-        MainMenuUIManager.Instance.onCreditsOpen += OnCreditsOpen;
-        MainMenuUIManager.Instance.onCreditsClose += OnCreditsClose;
+        if(SceneManager.GetActiveScene().name == ScenesManager.mainMenuSceneName){
+            MainMenuUIManager.Instance.onCreditsOpen += OnCreditsOpen;
+            MainMenuUIManager.Instance.onCreditsClose += OnCreditsClose;
+        }
+        else if (SceneManager.GetActiveScene().name == ScenesManager.gameOverSceneName){
+            EndGameUIManager.Instance.onCreditsOpen += OnCreditsOpen;
+            EndGameUIManager.Instance.onCreditsClose += OnCreditsClose;
+        }
+        
     }
 
     void OnDestory()
     {
-        MainMenuUIManager.Instance.onCreditsOpen -= OnCreditsOpen;
-        MainMenuUIManager.Instance.onCreditsClose -= OnCreditsClose;
+        if(SceneManager.GetActiveScene().name == ScenesManager.mainMenuSceneName){
+            MainMenuUIManager.Instance.onCreditsOpen -= OnCreditsOpen;
+            MainMenuUIManager.Instance.onCreditsClose -= OnCreditsClose;
+        }
+        else if (SceneManager.GetActiveScene().name == ScenesManager.gameOverSceneName){
+            EndGameUIManager.Instance.onCreditsOpen -= OnCreditsOpen;
+            EndGameUIManager.Instance.onCreditsClose -= OnCreditsClose;
+        }
     }
 
     void OnCreditsOpen()
--- QuadJam3/Assets/Scripts/UI/ModalMainMenu.cs
+++ QuadJam3/Assets/Scripts/UI/ModalMainMenu.cs
@@ -1,20 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ModalMainMenu : MonoBehaviour
 {
 
     void Start()
     {
-        MainMenuUIManager.Instance.onMainOpen += OnMainOpen;
-        MainMenuUIManager.Instance.onMainClose += OnMainClose;
+        if(SceneManager.GetActiveScene().name == ScenesManager.mainMenuSceneName){
+            MainMenuUIManager.Instance.onMainOpen += OnMainOpen;
+            MainMenuUIManager.Instance.onMainClose += OnMainClose;
+        }
+        else if (SceneManager.GetActiveScene().name == ScenesManager.gameOverSceneName){
+            EndGameUIManager.Instance.onMainOpen += OnMainOpen;
+            EndGameUIManager.Instance.onMainClose += OnMainClose;
+        }
     }
 
     void OnDestory()
     {
-        MainMenuUIManager.Instance.onMainOpen -= OnMainOpen;
-        MainMenuUIManager.Instance.onMainClose -= OnMainClose;
+        if(SceneManager.GetActiveScene().name == ScenesManager.mainMenuSceneName){
+            MainMenuUIManager.Instance.onMainOpen -= OnMainOpen;
+            MainMenuUIManager.Instance.onMainClose -= OnMainClose;
+        }
+        else if (SceneManager.GetActiveScene().name == ScenesManager.gameOverSceneName){
+            EndGameUIManager.Instance.onMainOpen -= OnMainOpen;
+            EndGameUIManager.Instance.onMainClose -= OnMainClose;
+        }
     }
 
     void OnMainOpen()
